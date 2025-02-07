afe9e7bd8208e16ed0e25fc9a546ffa30983285b
Jang seong hwan <jsksh9865@naver.com>
2025-02-07 22:40:25 +0900

    주석 추가
    문제 풀이 과정을 정리

--- .idea/Coding-Test/PrefixSum1.java
+++ .idea/Coding-Test/PrefixSum1.java
@@ -25,16 +25,39 @@ public class PrefixSum1 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        /*
+         * size : 입력 받을 데이터의 개수
+         * query : 질의 개수
+         */
         int size = Integer.parseInt(st.nextToken());
         int query = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int [] datas = new int[size+1];
 
+        /*
+         * datas 배열의 크기를 입력 받을 데이터의 개수보다 1이 많은 이유는
+         * 첫 번째 데이터부터 임의의 위치의 데이터까지의 구간 합을 구할 때
+         * 구간 합을 구하는 공식인 S[j] - S[i-1]을 사용하면 인덱스 범위를 넘기 때문이다(i-1이 음수가 되면 안되기 때문)
+         */
+        int [] datas = new int[size+1];
+        /*
+         * 아래의 for 문은 합 배열에 데이터를 넣으면서 이전에 구한 값(첫 번째 데이터부터 i-1번째 데이터까지의 합)을 더한다.
+         * 그렇기에 아래의 for 문으로 원본 배열을 만들지 않고 바로 합 배열을 만드는 것이다.
+         */
         for(int i = 1; i < datas.length; i++){
             datas[i] = Integer.parseInt(st.nextToken()) + datas[i-1];
         }
 
+        /*
+         * 아래의 for문은 구간 합을 구하는 과정이다.
+         * start는 구간의 시작, end는 구간의 마지막이다.
+         * bw(BufferedWriter)를 쓴 이유는 모든 질의에 대한 답을 구한 다음에 한 번에 출력할 수 있고
+         * System.out에 있는 print 함수보다 리소스를 적게 먹기 때문에 사용했다.
+         * 
+         * 이와 같은 원리로 BufferedReader도 Java.util에 있는 Scanner보다 리소스를 적게 먹는다.
+         * 
+         * BufferedReader와 BufferedWriter에 대해서는 따로 정리할 예정이다.
+         */
         for(int i = 0; i < query; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
