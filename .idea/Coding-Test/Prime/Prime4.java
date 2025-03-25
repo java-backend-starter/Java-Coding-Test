ebad09eea18c88f5b8f1cd8e9dbce8f32cbb39ac
Jang seong hwan <jsksh9865@naver.com>
2025-03-25 14:32:45 +0900

    Create Prime4.java

---
+++ .idea/Coding-Test/Prime/Prime4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Prime4 {
+    /*
+     * 최초 작성일시 : 2025-03-25
+     * 최초 작성시간 : 14:30
+     * 최초 작성자 : 정성환
+     *
+     * 문제 출처 : 백준
+     * 문제 번호 : 1016
+     * 문제 이름 : 소수&팰린드롬
+     * 문제 난이도 : 실버 Ⅰ
+     *
+     * 작성 목적
+     *
+     * 책에 있는 문제 복습
+     */
+    static void notSquare(long [] values){
+
+    }
+}
