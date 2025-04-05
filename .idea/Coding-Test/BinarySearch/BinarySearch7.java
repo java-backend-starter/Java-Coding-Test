e21010272a43db4bf6d1ab051a29399c6cdab108
Jang seong hwan <jsksh9865@naver.com>
2025-04-05 13:37:10 +0900

    최장 증가 부분 수열 개념 정리

--- .idea/Coding-Test/BinarySearch/BinarySearch7.java
+++ .idea/Coding-Test/BinarySearch/BinarySearch7.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class BinarySearch7 {
     /*
-     * 최초 작성일시 : 2025-03-1
-     * 최초 작성시간 :
+     * 최초 작성일시 : 2025-04-05
+     * 최초 작성시간 : 13:21
      * 최초 작성자 : 정성환
      *
      * 문제 출처 : 백준
@@ -16,4 +16,59 @@ public class BinarySearch7 {
      *
      * 백준에 있는 문제 풀이
      */
+    static int lowBounder(int [] values, int value){
+        int result = values.length;
+        int start = 0;
+        int end = values.length-1;
+
+        int mid;
+        while(start <= end){
+            mid = (start + end)/2;
+
+            if(values[mid] < value){
+                start = mid + 1;
+            }
+            else{
+                result = mid;
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    static int upperBound(int [] values, int value){
+        int result = values.length;
+        int start = 0;
+        int end = values.length - 1;
+
+        int mid;
+        while(start <= end){
+            mid = (start + end)/2;
+
+            if(values[mid] <= value){
+                start = mid + 1;
+            }
+            else {
+                result = mid;
+                end = mid - 1;
+            }
+        }
+    }
+
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int size = Integer.parseInt(br.readLine());
+        int [] values = new int[size];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < size; i++){
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(values);
+
+    }
 }
